49feb7359c2e6a399412a2f5b3b422f1189b0f1f
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-09-07 19:13:46 +0530

    some more db tables defined

--- src/accounts/domain/entities/models/public.go
+++ src/accounts/domain/entities/models/public.go
@@ -1,15 +1,16 @@
 package models
 
 type School struct {
-	ID       string `gorm:"primaryKey,id"`
-	Name     string `gorm:"name"`
-	Location string `gorm:"location"`
-	Address  string `gorm:"address"`
-	Logo     string `gorm:"logo"`
+	ID          string `gorm:"primaryKey,id"`
+	Name        string `gorm:"name"`
+	Location    string `gorm:"location"`
+	Address     string `gorm:"address"`
+	Logo        string `gorm:"logo"`
+	PhoneNumber string `gorm:"phone_number"`
 }
 
 type SuperAdmin struct {
 	ID             int    `gorm:"primaryKey,id"`
 	Username       string `gorm:"username"`
-	HashedPassword string `gorm:"hashedpassword"`
+	HashedPassword string `gorm:"hashed_password"`
 }
